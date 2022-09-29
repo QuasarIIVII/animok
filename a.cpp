2f51a8657dd1045fb3a1a5a4f34a1d7193f6ea2b
QuasarIIVII <quasariivii@gmail.com>
2022-09-29 18:06:04 +0900

    20220927B - GUI Initialized
    Init : GUI (Error Fixed)

--- a.cpp
+++ a.cpp
@@ -1,7 +1,11 @@
 #include<windows.h>
 
-typedef struct ltwh{SHORT l,t, w,h;}ltwh;//Left Top Width Height
-ltwh wndPos;
+typedef struct LTWH{SHORT l,t, w,h;}LTWH;//Left Top Width Height
+typedef union RECT$LTWH{
+	LTWH ltwh;
+	RECT rect;
+}RECT$LTWH;
+RECT$LTWH wndPos;
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam){
 	static struct{
 		HDC hdc, hmdc;
@@ -14,24 +18,24 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam){
 		}
 		case WM_CREATE:{
 			//Get Window Size
-			wndPos=GetWindowRect(hwnd,reinterpret_cast<LPRECT>(&wndPos));
-			wndPos={reinterpret_cast<RECT>(wndPos).left,
-			reinterpret_cast<RECT>(wndPos).top,
-			reinterpret_cast<RECT>(wndPos).bottom-reinterpret_cast<RECT>(wndPos).top,
-			reinterpret_cast<RECT>(wndPos).right-reinterpret_cast<RECT>(wndPos).left};
+			GetWindowRect(hwnd,&wndPos.rect);
+			wndPos={wndPos.rect.left,
+			wndPos.rect.top,
+			wndPos.rect.bottom-wndPos.rect.top,
+			wndPos.rect.right-wndPos.rect.left};
 
 			mem.hdc=GetDC(hwnd);
 			mem.hmdc=CreateCompatibleDC(mem.hdc);
-			mem.hmbmp=CreateCompatibleBitmap(mem.hdc,wndPos.w,wndPos.h);	//Compatible Bitmap
-			SelectObject(mem->hmdc,mem->hmbmp);
+			mem.hmbmp=CreateCompatibleBitmap(mem.hdc,wndPos.ltwh.w,wndPos.ltwh.h);	//Compatible Bitmap
+			SelectObject(mem.hmdc,mem.hmbmp);
 
 			break;
 		}
 		case WM_DESTROY: {
 			
-			DeleteObject(mem->hmbmp);
-			DeleteDC(mem->hmdc);
-			ReleaseDC(hwnd,mem->hdc);
+			DeleteObject(mem.hmbmp);
+			DeleteDC(mem.hmdc);
+			ReleaseDC(hwnd,mem.hdc);
 
 			PostQuitMessage(0);
 			break;
