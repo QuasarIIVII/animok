a1d2d1dd2038d62271bae2185a5a6c05aaf2b324
QuasarIIVII <quasariivii@gmail.com>
2022-12-06 07:44:32 +0900

    221206A - GUI : Mouse
    Add : Mouse movement

--- a.cpp
+++ a.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int memoryBitmapW;
 #define MoveToEx(DEST, T0, X, T1, T2) MoveToEx(T0, ((DEST)*memoryBitmapW) + (X), T1, T2)
 #define LineTo(DEST, T0, X, T1) LineTo(T0, ((DEST)*memoryBitmapW) + (X), T1)
-#define Rectangle(DEST, A, B, C, D, E) Rectangle(A, ((DEST)*memoryBitmapW) + (B), C, ((DEST)*memoryBitmapW) + (D), E);
+#define Rectangle(DEST, A, B, C, D, E) Rectangle(A, ((DEST)*memoryBitmapW) + (B), C, ((DEST)*memoryBitmapW) + (D), E)
+#define Ellipse(DEST, A, B, C, D, E) Ellipse(A, ((DEST)*memoryBitmapW) + (B), C, ((DEST)*memoryBitmapW) + (D), E)
 
 typedef struct LTWH{SHORT l,t, w,h;}LTWH;//Left Top Width Height
 typedef union RECT$LTWH{
@@ -29,11 +30,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam){
 	static struct S0{
 		COORD size, lt, num; //left-top, number of columns and rows
 	}grid;
+	static COORD pieceSize;
+	static COORD pos;
 	static PAINTSTRUCT ps;
 	switch(Message) {
 		case WM_PAINT:{
 			BeginPaint(hwnd, &ps);
-			BitBlt(mem.hmdc, grid.lt.X, grid.lt.Y, grid.size.X,grid.size.Y, mem.hmdc, memoryBitmapW * 1,0, SRCCOPY);
 			BitBlt(ps.hdc, 0,0, wndSzW,wndSzH, mem.hmdc, 0,0, SRCCOPY);
 			DefWindowProc(hwnd, Message, wParam, lParam);
 			EndPaint(hwnd, &ps);
@@ -41,6 +43,29 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam){
 		}
 		case WM_MOUSEMOVE:{
 //			cout<<"Mouse : "<<LOWORD(lParam)<<", "<<HIWORD(lParam)<<endl;
+			static COORD b={-1,-1};
+			pos={	(LOWORD(lParam)-grid.lt.X+(((grid.size.X)/(grid.num.X-1))>>1))*(grid.num.X-1)/(grid.size.X-1),
+					(HIWORD(lParam)-grid.lt.Y+(((grid.size.Y)/(grid.num.Y-1))>>1))*(grid.num.Y-1)/(grid.size.Y-1)};
+			if(b.X!=pos.X || b.Y!=pos.Y){
+				if(0<=pos.X && pos.X <grid.num.X && 0<=pos.Y && pos.Y <grid.num.Y){
+					b=pos;
+					SetDCBrushColor(mem.hmdc,0), SetDCPenColor(mem.hmdc, 0);
+					Rectangle(3, mem.hmdc, 0,0, wndSzW, wndSzH);
+//					Rectangle(0, mem.hmdc, 0,0, wndSzW, wndSzH);
+					BitBlt(mem.hmdc, memoryBitmapW*3 + pieceSize.X, pieceSize.Y, grid.size.X,grid.size.Y, mem.hmdc, memoryBitmapW * 1,0, SRCCOPY);
+					BitBlt(mem.hmdc, memoryBitmapW*3 + pos.X*(grid.size.X-1)/(grid.num.X-1)-(pieceSize.X>>1) + pieceSize.X, pos.Y*(grid.size.Y-1)/(grid.num.Y-1)-(pieceSize.Y>>1) + pieceSize.Y, pieceSize.X,pieceSize.Y, mem.hmdc, memoryBitmapW * 2,0, SRCCOPY);
+					BitBlt(mem.hmdc, memoryBitmapW*0 + grid.lt.X - pieceSize.X, grid.lt.Y - pieceSize.Y, grid.size.X + (pieceSize.X<<1),grid.size.Y + (pieceSize.Y<<1), mem.hmdc, memoryBitmapW * 3,0, SRCCOPY);
+					RECT rect={0,0,wndSzW, wndSzH};
+					InvalidateRect(hwnd, &rect, false);
+				}
+				else pos={-1, -1};
+			}
+			break;
+		}
+		case WM_LBUTTONDOWN:{
+			if(pos.X!=-1){
+			}
+			cout<<"click : "<<pos.X<<" "<<pos.Y<<endl;
 			break;
 		}
 		case WM_CREATE:{
@@ -70,7 +95,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam){
 			//Draw Grid
 			SetDCBrushColor(mem.hmdc, ARGB(0xffadff));
 			SetDCPenColor(mem.hmdc, ARGB(0xffffff));
-			Rectangle(0, mem.hmdc, 200, 200, 250, 250);
+//			Rectangle(0, mem.hmdc, 200, 200, 250, 250);
 			for(int i=grid.num.X-1; i+1;i--){
 				MoveToEx(1, mem.hmdc, (grid.size.X-1) * i / (grid.num.X-1), 0, nullptr);
 				LineTo(1, mem.hmdc, (grid.size.X-1) * i / (grid.num.X-1), grid.size.Y);
@@ -79,6 +104,16 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam){
 				MoveToEx(1, mem.hmdc, 0, (grid.size.Y-1) * i / (grid.num.Y-1), nullptr);
 				LineTo(1, mem.hmdc, grid.size.X, (grid.size.Y-1) * i / (grid.num.Y-1));
 			}
+
+			//Draw Pieces
+			pieceSize={50,50};
+			SetDCPenColor(mem.hmdc, ARGB(0xffffff));
+			SetDCBrushColor(mem.hmdc, ARGB(0x000000));
+			Ellipse(2,mem.hmdc, 0, 0, pieceSize.X, pieceSize.Y);
+			SetDCBrushColor(mem.hmdc, ARGB(0xffffff));
+			Ellipse(2,mem.hmdc, pieceSize.X, 0, pieceSize.X<<1, pieceSize.Y);
+
+			BitBlt(mem.hmdc, memoryBitmapW*0 + grid.lt.X, grid.lt.Y, grid.size.X,grid.size.Y, mem.hmdc, memoryBitmapW * 1,0, SRCCOPY);
 			break;
 		}
 		case WM_DESTROY: {
@@ -99,6 +134,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam){
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+//	FreeConsole();
 	cout<<"S : "<<GetSystemMetrics(SM_CXSCREEN)<<", "<<GetSystemMetrics(SM_CYSCREEN)<<endl;
 	WNDCLASSEX wc;
 	HWND hwnd;
